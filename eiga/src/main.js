4e63ceb4aa14b5607f990ac588f333c399a8ff3a
Jonah Simpson <jonahs99@gmail.com>
2019-04-07 09:11:59 -0400

    vuefire fix

--- eiga/src/main.js
+++ eiga/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueFire from 'vuefire'
+import { firestorePlugin } from 'vuefire'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 Vue.config.productionTip = false
 
-Vue.use(VueFire)
+Vue.use(firestorePlugin)
 firebase.initializeApp({
   projectId: 'eiga19',
   databaseURL: 'https://eiga19.firebaseio.com'
@@ -14,9 +14,16 @@ firebase.initializeApp({
 
 export const db = firebase.firestore()
 
+/*Vue.use(Vuefire.firestorePlugin)
+var db = firebase
+.initializeApp({
+  projectId: 'vue-fire-store',
+  databaseURL: 'https://vue-fire-store.firebaseio.com',
+})
+.firestore()
+db.settings({ timestampsInSnapshots: true })*/
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-console.log(VueFire)
 
